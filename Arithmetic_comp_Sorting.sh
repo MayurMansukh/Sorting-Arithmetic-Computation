b54411e60a29491742e50360ba51d2236ad74446
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-06 11:49:09 +0530

    get result in decending order

--- Arithmetic_comp_Sorting.sh
+++ Arithmetic_comp_Sorting.sh
@@ -60,4 +60,9 @@ echo "The computation expressions result are"
 for i in ${!expressionResultDict[@]}
 do
     echo $i"="${expressionResultDict[$i]}
+    expressionResultArray[((counter++))]=${expressionResultDict[$i]}
+
 done
+echo "The computation expressions result in Descending"
+sortedDesc=($(echo ${expressionResultArray[*]}| tr " " "\n" | sort -nr))
+echo ${sortedDesc[@]}
